53b66ea653a1aec9470611f895984514e0a75a3b
Aditya Garg <aditya26026@gmail.com>
2021-03-11 15:16:19 +0530

    added js files

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+import { handleVerify } from '.verify'
+import Router from './router'
+
 addEventListener('fetch', event => {
+  // const url = new URL(request.url)
+  // if (request.method === 'OPTIONS') {
+  //   // Handle CORS preflight requests
+  //   event.respondWith(handleOptions(request))
+  // } else if (
+  //     request.method === 'GET' ||
+  //     request.method === 'HEAD' ||
+  //     request.method === 'POST'
+  // ) {
+  //     // Handle requests to the API server
+  //     event.respondWith(handleRequest(request))
+  // } else {
+  //     event.respondWith(
+  //         new Response(null, {
+  //             status: 405,
+  //             statusText: 'Method Not Allowed',
+  //         })
+  //     )
+  // }
   event.respondWith(handleRequest(event.request))
 })
 /**
@@ -6,7 +28,11 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
+  const r = new Router()
+  r.get('.*/verify', request => handleVerify(request))
   return new Response('Hello worker!', {
     headers: { 'content-type': 'text/plain' },
   })
+
+
 }
