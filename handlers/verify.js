9ebcae9b310098ca863bf535814e3fafa83aac6a
ayush2843 <33235626+ayush2843@users.noreply.github.com>
2021-03-15 13:07:05 +0530

    added sublime tweet verifier

--- handlers/verify.js
+++ handlers/verify.js
@@ -8,7 +8,7 @@ const USER_AGENT = 'Cloudflare Worker'
 
 // format request for twitter api
 
-const TWITTER_BEARER = "AAAAAAAAAAAAAAAAAAAAACNTLAEAAAAAerwwVRSbIdpeHLBzGJsEOomFiWo%3DvldHFkYQToX1rgurKBp486DzVJ0octmf5RXdfSOrcn1VxNtpOa";
+const TWITTER_BEARER = "";
 var requestHeaders = new Headers()
 requestHeaders.append('Authorization', 'Bearer ' + TWITTER_BEARER)
 var requestOptions = {
@@ -21,18 +21,9 @@ const init = {
 }
 
 // regex for parsing tweet
-const reg = new RegExp('(?<=sig:).*')
+const regsig = new RegExp('(?<=sig:).*')
+// const regadd = new RegExp('(?<=addr:)([0x[:xdigit:]])*')
 
-/**
- * @param {*} request
- * Accpets id=<tweet id>
- * Accepts account=<eth address> // just used to aler client of incorrect signer found
- *
- * 1. fetch tweet data using tweet id
- * 2. construct signature data using handle from tweet
- * 3. recover signer of signature from tweet
- * 4. if signer is the expected address, update gist with address -> handle mapping
- */
 export async function handleVerify(request) {
     try {
         
@@ -45,17 +36,25 @@ export async function handleVerify(request) {
         requestOptions.headers.set('Origin', new URL(twitterURL).origin) // format for cors
         const twitterRes = await fetch(twitterURL, requestOptions)
         const twitterResponse = await gatherResponse(twitterRes)
-        
+        const tweetContent = twitterResponse.data[0].text
+
+        var matchedSig = tweetContent.match(regsig);
+        // var matchedAdd = tweetContent.match(regadd);
         if (!twitterResponse.data || !twitterResponse.includes) {
             return new Response(null, {
                 status: 400,
                 statusText: 'Invalid tweet id',
             })
         }
-        const tweetContent = twitterResponse.data[0].text
-        var new_tweet  = tweetContent.split(' signature ');
-        var sig = new_tweet[1];
+        if( !matchedSig){
+            return new Response(null, {
+                status:400,
+                statusText: 'Tweet is not proper'
+            })
+        }
         const handle = twitterResponse.includes.users[0].username
+        const sig = matchedSig[0];
+        console.log(sig);
         const msgParams = {
             types: {
               EIP712Domain: [
@@ -84,7 +83,7 @@ export async function handleVerify(request) {
             message: {
               sender: {
                 name: handle,
-                wallet: new_tweet[0],
+                wallet: account,
               },
               recipient: {
                 name: 'Sublime',
@@ -107,7 +106,6 @@ export async function handleVerify(request) {
             })
         }
     
-        // if signer found is not the expected signer, alert client and dont update gist
         if (account === recovered) {
             response = new Response(handle, init, {
                 status: 200,
@@ -118,7 +116,6 @@ export async function handleVerify(request) {
             return response
         }
         else{
-            
             return new Response(null, init, {
                 status: 400,
                 statusText: 'Invalid account',
